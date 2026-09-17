ddfa5615b1abcddb2f91c7a762bbefb173de7bdd
Delta4719 <28107@ud.ac.th>
2026-09-17 09:45:08 +0700

    Refactor input handling and discount calculation
    Update input prompts and improve member discount logic.

--- Examination_3.py
+++ Examination_3.py
@@ -6,5 +6,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-price = float(input())
-is_member = int(input())
+price = float(input("ราคาสินค้าเต็ม: "))
+is_member = int(input("สถานะสมาชิก: "))
+
+if is_member is 1:
+    print (price - (price * 10 / 100))
+elif is_member is 0:
+    print (price)
